299b4a43940f2b7b28e4406a985d366557ea1a9f
TestLeafPages <sarath.muthuswamy@qeagle.com>
2022-01-01 19:24:34 +0530

    Add files via upload

---
+++ week6/day1/steps/StepDefinition.java
@@ -0,0 +1,64 @@
+package steps;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.But;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class StepDefinition {
+	public ChromeDriver driver;
+
+	@Given("Open the chrome browser")
+	public void openBrowser() {
+		WebDriverManager.chromedriver().setup();
+		driver  = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	}
+	
+	@Given("Load the application url")
+	public void loadApplication() {
+		driver.get("http://leaftaps.com/opentaps/");
+	}
+
+	@Given("Enter the username as {string}")
+	public void enterUsername(String username) {
+		driver.findElement(By.id("username")).sendKeys(username);
+	}
+
+	@Given("Enter the password as {string}")
+	public void enterPassword(String password) {
+		driver.findElement(By.id("password")).sendKeys(password);
+	}
+
+	@When("Click on login button")
+	public void clickLoginButton() {
+		driver.findElement(By.className("decorativeSubmit")).click();
+	}
+
+	@Then("Homepage should be displayed")
+	public void verifyHomepage() {
+		boolean displayed = driver.findElement(By.linkText("CRM/SFA")).isDisplayed();
+		if(displayed) {
+			System.out.println("Homepage is displayed");
+		}else {
+			System.out.println("Homepage is not displayed");
+			
+		}
+	}
+	
+	@But("Error message should be displayed")
+	public void verifyErrorMessage() {
+		System.out.println("Error message is displayed");
+
+	}
+
+	
+}
